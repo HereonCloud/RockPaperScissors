5d0af1f0ce368cf3843be1e4e8f22e8496ffd478
Cleo <cprubino25@gmail.com>
2023-12-17 20:19:01 +0800

    Add assets and html, css and js codes

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-function generateComputerPick(){
-    const options = ["scissors", "rock", "paper"]
-    return options[Math.floor(Math.random() * options.length)]
+function generateComputerPick() {
+  const options = ["scissors", "rock", "paper"];
+  return options[Math.floor(Math.random() * options.length)];
 }
 
-function checkWinner(player, opponent){
-    if(player  === opponent) return "DRAW"
-    if((player === "scissors" && opponent === "rock") || (player === "paper" && opponent === "scissors") || (player === "rock" && opponent ==="paper")) return "YOU LOSE"
-    return "YOU WIN"
+function checkWinner(player, opponent) {
+  if (player === opponent) return "DRAW";
+  if (
+    (player === "scissors" && opponent === "rock") ||
+    (player === "paper" && opponent === "scissors") ||
+    (player === "rock" && opponent === "paper")
+  )
+    return "YOU LOSE";
+  return "YOU WIN";
 }
 
- document.querySelectorAll("button").forEach((e) => e.addEventListener("click", (b) => {
-    var player = b.target.value
-    var opponent = generateComputerPick()
-    document.getElementById("opponent").style.backgroundImage = `url("/assets/images/${opponent}.svg")`
-    document.getElementById("player").style.backgroundImage = `url("/assets/images/${player}.svg")`
-    document.getElementById("outcome").innerText = checkWinner(player, opponent)
-}))
-
-
-
+document.querySelectorAll("button").forEach((e) =>
+  e.addEventListener("click", (b) => {
+    var player = b.target.value;
+    var opponent = generateComputerPick();
+    document.getElementById(
+      "opponent"
+    ).style.backgroundImage = `url(assets/images/${opponent}.svg)`;
+    document.getElementById(
+      "player"
+    ).style.backgroundImage = `url(assets/images/${player}.svg)`;
+    document.getElementById("outcome").innerText = checkWinner(
+      player,
+      opponent
+    );
+  })
+);
